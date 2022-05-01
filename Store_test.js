77de2155fb14da9c7b94ea7fb4987f6697364fc8
RiaboshlykVeronika <82654040+RiaboshlykVeronika@users.noreply.github.com>
2022-05-01 23:54:00 +0300

    add new pages

--- Store_test.js
+++ Store_test.js
@@ -1,9 +1,23 @@
+let user = {
+  firstName: 'Test',
+  lastName: 'Automation',
+  password: '11111',
+  state: 'Alabama',
+  city: 'Birmingham',
+  address: '801 Tom Martin Dr.',
+  postalCode: '35211',
+  mobilePhone: '+380930000000',
+  asignAdress: 'My adress', 
+}
+
 Feature('Store');
 
-Scenario('test something', ({ I }) => {
-    I.amOnPage('http://automationpractice.com/index.php');
-    pause();
-    I.see('Women');
-    pause();
+Scenario('test something', ({ I, homePage, authPage, createAccountPage }) => {
+  homePage.openStore();
+  homePage.clickSignIn();
+  authPage.fillNewUserEmail(Date.now() + '@test.com');
+  authPage.clickCreateAccount();
+  createAccountPage.fillNewUserForm(user);
+  createAccountPage.clickSubmit();
+  pause()
 });
-
